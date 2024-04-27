4bb84552755547c56ee76a253069bfb0af4ab16a
duongtran2004 <S3979381@rmit.edu.vn>
2024-04-27 14:57:12 +0700

    Add GenerateRandomInteger Apllication

---
+++ src/main/java/com/example/javafxpracticeexercises/RandomNumberGeneratorApplication.java
@@ -0,0 +1,62 @@
+package com.example.javafxpracticeexercises;
+//import necessary stuffs
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.util.Random;
+
+public class RandomNumberGeneratorApplication extends Application { //every JavaFX Application must extend the Application Class
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //add a control (control in this case is a button)
+        Button button1 = new Button("Click Me To Generate A Random Number");
+        //define the layout (why root?)
+        HBox rootLayout = new HBox();
+        //we need to add our control
+        rootLayout.getChildren().add(button1);
+        //add this layout to the scene
+        Scene scene = new Scene(rootLayout);
+        //we need to add this scene to stage
+        primaryStage.setScene(scene);
+
+        //Event Handiling
+        // Set an action when the button is clicked.
+        button1.setOnAction(event -> {
+            // You can choose to either display an alert:
+            showAlert("Button Clicked", "Number generated :" + generateRandomInteger());
+
+            // OR change the button text:
+            // actionButton.setText("Clicked!");
+        });
+
+
+        // Set the title of the window.
+        primaryStage.setTitle("Button Action App");
+
+        // Show the window.
+        primaryStage.show();
+    }
+
+//helper method to generate a random number
+
+    private int generateRandomInteger(){
+        Random random = new Random();
+        int randomInt = random.nextInt(10);
+        return randomInt;
+    }
+    // Helper method to show an alert dialog.
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+}
+
